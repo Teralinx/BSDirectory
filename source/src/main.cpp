9220d1942a478555bd5e348ddbfb1b1996676070
teralin <ty2011x@gmail.com>
2026-06-21 23:32:15 +0800

    增加QT并可调试

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <QApplication>
 int main(int argc, char** argv){
-    std::cout<< "111111111111" << std::endl;
+    QApplication a(argc, argv);
+    QString ss = "sdfdsfsd";
+    std::cout << ss.toStdString() << std::endl;
+    a.exec();
     return 0;
 }
